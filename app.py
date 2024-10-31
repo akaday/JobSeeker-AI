e72462cd9683d1186f0e0f70015805367ac082d5
aka <akadev8@outlook.com>
2024-10-31 08:08:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from endpoints import router as jobs_router
 app = FastAPI()
 
 app.include_router(jobs_router, prefix="/api")
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to JobSeeker AI!"}
